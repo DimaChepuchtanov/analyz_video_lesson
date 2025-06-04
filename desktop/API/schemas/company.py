65db20995b1122ab915e7763ea0f18a3cc81af4b
Pythoner-Umka <112894460+DimaChepuchtanov@users.noreply.github.com>
2025-06-04 15:32:03 +0500

    Delete desktop directory

--- desktop/API/schemas/company.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-import re
-
-
-class SchemaCompany(BaseModel):
-    name: str
-    director: str
-    phone: str
-    license: int
--- desktop/API/schemas/file.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from fastapi import HTTPException
-
-
-class SchemaFile(BaseModel):
-    path: str = "None"
-    duration: str = '00:00:00'
-    size: str = '0 bytes'
-    text_data: str = "None"
